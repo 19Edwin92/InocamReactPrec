900e42ec48e2442ae5b9ce9d7b11472a541ecfe9
19Edwin92 <b_a_s_e@naver.com>
2023-06-29 13:20:08 +0900

    [tsx] week-02 Redux => toolkit

--- InocamReactTS/README.md
+++ InocamReactTS/README.md
@@ -5,3 +5,4 @@
 |styled-components|yarn add styled-components @types/styled-components|
 |react-router-dom|yarn add react-router-dom @types/react-router-dom|
 |redux|yarn add redux react-redux @types/react-redux|
+|reudx toolkit|yarn add @reduxjs/toolkit|
--- InocamReactTS/src/redux/config/configStore.tsx
+++ InocamReactTS/src/redux/config/configStore.tsx
@@ -1,7 +1,20 @@
-import { combineReducers, legacy_createStore as createStore } from "redux";
+// import { combineReducers, legacy_createStore as createStore } from "redux";
+// import todoReducer from '../modules/todoReducer'
+
+// const rootReducers = combineReducers({todoReducer})
+// const store = createStore(rootReducers)
+
+// export default store
+
+// Redux Toolkit 으로 업하기 
+import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from '../modules/todoReducer'
 
-const rootReducers = combineReducers({todoReducer})
-const store = createStore(rootReducers)
+const store = configureStore({
+  reducer : {
+    todoReducer
+  }
+})
 
+export type RootState = ReturnType<typeof store.getState>
 export default store
