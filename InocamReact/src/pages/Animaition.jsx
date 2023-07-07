71a63ebaacab4a47c2cc0ef84641ab022c5bb2ec
19Edwin92 <b_a_s_e@naver.com>
2023-07-07 22:44:41 +0900

    [jsx]animation

--- InocamReact/src/pages/Animaition.jsx
+++ InocamReact/src/pages/Animaition.jsx
@@ -1,7 +1,10 @@
 import React, { useRef, useState } from "react";
-import { css, styled } from "styled-components";
+import { css, styled, keyframes } from "styled-components";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { coldarkDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import Backgroung from '../img/conam/bikebg.png'
+import Bike from '../img/conam/bike.png'
+import Tire from '../img/conam/tire.png'
 
 function Animaition() {
   const inputRef = useRef(null);
@@ -38,6 +41,62 @@ function Animaition() {
       width: 240px;
   \`};
 
+  \`
+  `;
+
+  const codeBlock3 = `
+  // transform: rotateY(180deg)을 활용한 간단한 카드 애니메이션이다.
+  // transform-style: preserve-3d; 또한 해당 속성은 하위 태그에게 3D공간을 묘사하도록 한다.(아버지 컴포넌트)
+  // perspective: 1000px; 이를 위해 할아버지컴포넌트(2D)에 원근감을 부여하였다.
+  // backface-visibility: hidden; 뒤집힌 상태에서는 화면에 노출되지 않도록 설정하였다. 
+
+  const CardWrap = styled.div\`
+    width: 100%;
+    height: 100%;
+    position: relative;
+    color: white;
+    text-align: center;
+    line-height: 400px;
+    transform-style: preserve-3d;
+    transform: rotateY(0deg);
+    transition: .5s;
+    \`
+
+  const Card = styled.div\`
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    backface-visibility: hidden;
+    background: skyblue;
+
+    \${({direction}) => direction === "back" && css\`
+      background: orange;
+      transform: rotateY(180deg);
+      \`}
+      \`
+
+  const CardLayout = styled.div\`
+      width: 300px;
+      height: 400px;
+      perspective: 1000px;
+
+  &:hover \${CardWrap} {
+      transform: rotateY(-180deg);
+  }
+  \`
+  `;
+
+  const codeBlock4 = `
+  // 배경이 움직이고, 바퀴가 움직이는 것 역시 keyframes 으로 구현할 수 있다.
+  const tire = keyframes\`
+    0% {transform: rotate(0)}
+    100% {transform:rotate(360deg)}
+  \`
+  const movebg =  keyframes\`
+    0% {background-position: 0 center}; 
+    /* 기본위치 */
+    100% {background-position: -1500px center};
+    /* -500px로 값이 바뀌도록 설정하였다. */
   \`
   `;
 
@@ -87,7 +146,6 @@ function Animaition() {
         }
       />
       <h2>카드 뒤집기</h2>
-      <div style={{display:"flex"}}>
       <CardLayout>
         <CardWrap>
             <Card>앞면</Card>
@@ -100,14 +158,85 @@ function Animaition() {
             language="javascript"
             style={coldarkDark}
             size="half"
-            children={codeBlock2}
+            children={codeBlock3}
           />}/>
-      </div>
+
+          {/* {/* 배경이 흐르는 듯한 이미지 */}
+          <BackgroundWave>
+            <BikeLayout>
+              <BikeImg src={Bike} alt="bike"/>
+              <TireImg1 src={Tire} alt="Tire"/>
+              <TireImg2 src={Tire} alt="Tire"/>
+            </BikeLayout>
+          </BackgroundWave>
+          <StyledSyntaxHighlighter
+        children={
+          <SyntaxHighlighter
+            language="javascript"
+            style={coldarkDark}
+            children={codeBlock4}
+          />
+        }
+      />
     </div>
   );
 }
 
 export default Animaition;
+
+const tire = keyframes`
+  0% {transform: rotate(0)}
+  100% {transform:rotate(360deg)}
+`
+const movebg =  keyframes`
+  0% {background-position: 0 center}; 
+  /* 기본위치 */
+  100% {background-position: -1500px center};
+  /* -500px로 값이 바뀌도록 설정하였다. */
+`
+
+
+const BikeLayout = styled.div`
+  position: relative;
+  width: 800px;
+  height: 500px;
+`
+
+const BikeImg = styled.img`
+  position:absolute;
+  top:66%;
+  left:50%;
+  z-index: 1;
+  transform: translate(-50%, -50%);
+`
+
+const TireImg1 = styled.img`
+  position:absolute;
+  bottom: 0;
+  right: 25%;
+  transform: translateX(-50%);
+  animation: ${tire} 1s linear infinite;
+`
+
+
+const TireImg2 = styled.img`
+  position:absolute;
+  bottom: 0;
+  left: 29%;
+  transform: translateX(-50%);
+  animation: ${tire} 1s linear infinite;
+`
+
+
+const BackgroundWave = styled.div`
+
+  width: 800px;
+  height: 500px;
+  background: url(${Backgroung}) 0 center / 1500px repeat-x;
+  /* 0 center 배경위미지의 위치 중앙정렬, 이미지 사이즈,x축에 대한 반복 */
+  animation: ${movebg} 2s linear infinite ;
+  /* linear 일정한 속도로, infinite 무한반복  */
+`
 
 const CardWrap = styled.div`
     width: 100%;
