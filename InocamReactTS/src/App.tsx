96c3367487d938e80fae2ef0fcc769cd1d1165cc
19Edwin92 <b_a_s_e@naver.com>
2023-08-08 23:40:31 +0900

    [react-chartjs-2] 테스트

--- InocamReactTS/src/App.tsx
+++ InocamReactTS/src/App.tsx
@@ -2,12 +2,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import TestChart from './pages/TestChart'
 
 const App: React.FC = () => {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/testchart' element={<TestChart/>}/>
       </Routes>
     </div>
   )
